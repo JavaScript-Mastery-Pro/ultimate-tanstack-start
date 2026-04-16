d1ea86212ffc083f57ba6d1c47416e280416005c
sujatagunale <sujata@jsmastery.pro>
2026-04-16 12:45:59 +0530

    feat: splat routes

---
+++ routing/src/routes/docs/$.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/docs/$")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { _splat } = Route.useParams();
+
+  return <div>Path: /docs/{_splat}</div>;
+}
